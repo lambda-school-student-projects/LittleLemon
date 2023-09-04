d884c9eff33d39a5dc94d5a86f7f0e17fda6da4d
Joshua Kaunert <44586402+jkaunert@users.noreply.github.com>
2023-09-04 13:20:23 -0500

    adds DRF token generation

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('menu/', views.MenuItemsView.as_view()),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
-    path('api/auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api-token-auth/', obtain_auth_token),
 ]
