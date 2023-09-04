f10fb03a405aba5cf4d3c4c09df1a9a1ce081416
Joshua Kaunert <44586402+jkaunert@users.noreply.github.com>
2023-09-04 11:09:23 -0500

    renames Menu model to MenuItem

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Menu(models.Model):
+class MenuItem(models.Model):
     id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -9,6 +9,7 @@ class Menu(models.Model):
     
     def __str__(self):
         return self.title
+    
 
 class Booking(models.Model):
     id = models.IntegerField(primary_key=True)
