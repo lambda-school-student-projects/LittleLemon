081c047f661e6820fa4049e54d80eaf814f8a4e8
Joshua Kaunert <44586402+jkaunert@users.noreply.github.com>
2023-09-04 13:20:41 -0500

    updates permission_classes attribute

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -16,11 +16,14 @@ class UserViewSet(viewsets.ModelViewSet):
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated]
