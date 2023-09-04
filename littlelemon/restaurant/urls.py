20db3fd46a090c24c44054223daaee91636f8d99
Joshua Kaunert <44586402+jkaunert@users.noreply.github.com>
2023-09-04 11:10:28 -0500

    updates urlConf

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from . import views
+
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('menu/', views.MenuItemsView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('api/auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
